80661db0ef57b27fb3fa51d777823ba661d120e8
hanan <hanan_rahman_23rpl@student.smktelkom-mlg.sch.id>
2016-10-09 10:04:25 +0700

    Inisialisasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas1/xiirpl1020/formpendaftaranbeasiswa/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xiirpl1020/formpendaftaranbeasiswa/MainActivity.java
@@ -11,17 +11,33 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewNama, textViewHobi, textViewJurusan, textViewAlamat;
-    EditText editTextNama, editTextAlamat;
-    RadioButton radioButtonLk, radioButtonWn;
-    CheckBox CBNyanyi, CBGambar, CBOlah, CBBaca;
-    Spinner spinnerJurusan;
-    Button buttonOK;
+    TextView TVnama, TVHobi, TVJurusan, TVAlamat;
+    EditText ETNama, ETAlamat;
+    RadioButton RBLk, RBWn;
+    CheckBox CBNyan, CBGam, CBOl, CBBa;
+    Spinner spinnerJurus;
+    Button butOK;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TVnama = (TextView) findViewById(R.id.textViewNama);
+        TVHobi = (TextView) findViewById(R.id.textViewHobi);
+        TVJurusan = (TextView) findViewById(R.id.textViewJurusan);
+        TVAlamat = (TextView) findViewById(R.id.textViewAlamat);
+        ETNama = (EditText) findViewById(R.id.editTextNama);
+        ETAlamat = (EditText) findViewById(R.id.editTextAlamat);
+        RBLk = (RadioButton) findViewById(R.id.radioButtonLk);
+        RBWn = (RadioButton) findViewById(R.id.radioButtonWn);
+        CBNyan = (CheckBox) findViewById(R.id.CBNyanyi);
+        CBGam = (CheckBox) findViewById(R.id.CBGambar);
+        CBOl = (CheckBox) findViewById(R.id.CBOlah);
+        CBBa = (CheckBox) findViewById(R.id.CBBaca);
+        spinnerJurus = (Spinner) findViewById(R.id.spinnerJurusan);
+        butOK = (Button) findViewById(R.id.buttonOK);
     }
+
 }
